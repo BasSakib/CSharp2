c2b04fedcc330e0fc57d9b9a5067d9c8783af45e
sohan0019 <sohanurrahman3092@gmail.com>
2025-11-05 09:05:05 +0600

    New Branch created

---
+++ function.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int rectangle(int a,int b)
+{
+    int area=0;
+    area= a*b;
+
+}
+int circle(float p=3.14, int r)
+{
+    int area=0;
+    area= p*r;
+}
+int square(int side)
+{
+    int area=0;
+    area= side* side;
+}
